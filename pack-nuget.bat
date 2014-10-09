def3c38e035b8838f80ae559671d22d5ea842b2d
Uriel Hai <urielha@users.noreply.github.com>
2014-10-09 20:18:40 +0300

    pack-nuget.bat

--- pack-nuget.bat
+++ pack-nuget.bat
@@ -1,3 +1,4 @@
+rmdir /S bin
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe src\log4net.ElasticSearch.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
 %~dp0src\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe -noxml -nodots -labels %~dp0src\log4net.ElasticSearch.Tests\bin\Release\log4stash.Tests.dll
 
