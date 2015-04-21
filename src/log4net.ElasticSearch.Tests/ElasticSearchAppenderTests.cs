27899bc82f0b0630b780dfd0d52d89570fccbda2
Uriel Hai <urielha@users.noreply.github.com>
2015-04-22 00:02:42 +0300

    Add UnitTest for SSL
    this is the most I can get for now

--- src/log4net.ElasticSearch.Tests/ElasticSearchAppenderTests.cs
+++ src/log4net.ElasticSearch.Tests/ElasticSearchAppenderTests.cs
@@ -4,13 +4,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using log4net.Core;
 using log4net.ElasticSearch.Filters;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace log4net.ElasticSearch.Tests
 {
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void Ssl_should_create_https()
+        {
+            const string expectedUrl = "https://server:8080/";
+            var client = new WebElasticClient("server", 8080, true, true, "username", "password");
+            Assert.AreEqual(expectedUrl, client.Url);
+        }
+    }
+
     [TestFixture]
     public class ElasticsearchAppenderTests : TestsSetup
     {
