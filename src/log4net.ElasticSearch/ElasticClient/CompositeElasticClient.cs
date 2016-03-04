7692408ea44dfdf9f4ac77dd882dbe3cbc7f388c
Uriel Hai <urielha@users.noreply.github.com>
2016-03-04 15:19:57 +0200

    (partial work) add CompositeElasticClient

---
+++ src/log4net.ElasticSearch/ElasticClient/CompositeElasticClient.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace log4net.ElasticSearch
+{
+    // TODO: need more work here
+    public class CompositeElasticClient : IElasticsearchClient
+    {
+        private int _current;
+        private readonly List<IElasticsearchClient> _clients;
+        
+        public string Server { get; private set; }
+        public int Port { get; private set; }
+        public bool Ssl { get; private set; }
+        public bool AllowSelfSignedServerCert { get; private set; }
+        public string BasicAuthUsername { get; private set; }
+        public string BasicAuthPassword { get; private set; }
+
+        public CompositeElasticClient(int clients, Func<IElasticsearchClient> factory)
+        {
+            _current = 0;
+            _clients = new List<IElasticsearchClient>(clients);
+            for (int i = 0; i < clients; i++)
+            {
+                _clients.Add(factory());
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var client in _clients)
+            {
+                client.Dispose();
+            }
+        }
+
+        public void PutTemplateRaw(string templateName, string rawBody)
+        {
+            int i = Interlocked.Increment(ref _current);
+            _clients[i].PutTemplateRaw(templateName, rawBody);
+        }
+
+        public void IndexBulk(IEnumerable<InnerBulkOperation> bulk)
+        {
+            int i = Interlocked.Increment(ref _current);
+            _clients[i].IndexBulk(bulk);
+        }
+
+        public IAsyncResult IndexBulkAsync(IEnumerable<InnerBulkOperation> bulk)
+        {
+            int i = Interlocked.Increment(ref _current);
+            return _clients[i].IndexBulkAsync(bulk);
+        }
+    }
+}
