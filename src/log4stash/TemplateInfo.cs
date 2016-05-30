5ba5baa43c6ff411052c23c42749d9bf0d8244c9
Uriel Hai <urielha@users.noreply.github.com>
2016-05-30 11:43:38 +0300

    sends errors to LogLog and return on failure

--- src/log4stash/TemplateInfo.cs
+++ src/log4stash/TemplateInfo.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.IO;
 using log4net.Core;
 using log4net.Util;
@@ -6,8 +7,6 @@ namespace log4stash
 {
     public class TemplateInfo : IOptionHandler
     {
-        private readonly IErrorHandler _errorHandler;
-
         public string Name { get; set; }
         public string FileName { get; set; }
         public bool IsValid { get; private set; }
@@ -15,19 +14,20 @@ namespace log4stash
         public TemplateInfo()
         {
             IsValid = false;
-            _errorHandler = new OnlyOnceErrorHandler(this.GetType().Name);
         }
 
         public void ActivateOptions()
         {
             if (string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(FileName))
             {
-                _errorHandler.Error("Template name or fileName is empty!");
+                LogLog.Error(GetType(), "Template name or fileName is empty!");
+                return;
             }
 
             if (!File.Exists(FileName))
             {
-                _errorHandler.Error(string.Format("Could not find template file: {0}", FileName));
+                LogLog.Error(GetType(), string.Format("Could not find template file: {0}", FileName));
+                return;
             }
 
             IsValid = true;
