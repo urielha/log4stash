876791882f97c02f26ffd476375e8b5ef339bb6e
Uriel Hai <urielha@users.noreply.github.com>
2016-11-27 00:14:21 +0200

    travis changes

--- src/.ci/RunTests.sh
+++ src/.ci/RunTests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
+mono .nuget/NuGet.exe install NUnit.Runners -Version 2.6.3 -OutputDirectory packages
 
 runTest(){
-    mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels $@
+    mono packages/NUnit.Runners.2.6.3/tools/nunit-console.exe -noxml -nodots -labels $@
    if [ $? -ne 0 ]
    then   
      exit 1
