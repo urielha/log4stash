fee1867cc921657433a5e02edaebced19e2f727c
Uriel Hai <urielha@users.noreply.github.com>
2018-08-08 22:03:06 +0300

    ToleratedCalls

---
+++ src/log4stash/Extensions/ToleratedCalls.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace log4stash.Extensions
+{
+    /// <summary>
+    /// Tolerating calls to function in given timespan.
+    /// First call to <see cref="Call"/> with given type and hint key will call the action.
+    /// After that, all calls (with same type and key) will be dropped till the timespan is over.
+    /// 
+    /// The function <see cref="Call"/> is thread safe.
+    /// </summary>
+    public class ToleratedCalls
+    {
+        private readonly TimeSpan _tolerance;
+        private readonly ConcurrentDictionary<Tuple<Type, int>, DateTime> 
+            _errorsHistory = new ConcurrentDictionary<Tuple<Type, int>, DateTime>();
+
+        public ToleratedCalls(TimeSpan tolerance)
+        {
+            _tolerance = tolerance;
+        }
+
+        public void Call(Action action, Type type, int hint)
+        {
+            var tup = new Tuple<Type, int>(type, hint);
+            var now = DateTime.Now;
+            if (_errorsHistory.TryAdd(tup, now))
+            {
+                action();
+                return;
+            }
+
+            var lastErr = _errorsHistory[tup];
+            if (now - lastErr > _tolerance)
+            {
+                if (_errorsHistory.TryUpdate(tup, now, lastErr))
+                {
+                    action();
+                }
+            }
+        }
+    }
+}
