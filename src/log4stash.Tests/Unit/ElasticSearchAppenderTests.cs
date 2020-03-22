71ecbbda27c5f7fdcb282c4b8a5fcfe0ca7188de
Eran Gil <eran.gil2@gmail.com>
2020-03-22 15:35:35 +0200

    Added first tests for elastic search appender

---
+++ src/log4stash.Tests/Unit/ElasticSearchAppenderTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4stash.Bulk;
+using log4stash.Extensions;
+using log4stash.Timing;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace log4stash.Tests.Unit
+{
+    class ElasticSearchAppenderTests
+    {
+        private IElasticsearchClient _elasticClient;
+        private ITolerateCallsFactory _tolerateCallsFactory;
+        private IIndexingTimer _timer;
+        private ILogBulkSet _bulk;
+
+        [SetUp]
+        public void Setup()
+        {
+            _elasticClient = Substitute.For<IElasticsearchClient>();
+            _timer = Substitute.For<IIndexingTimer>();
+            _tolerateCallsFactory = Substitute.For<ITolerateCallsFactory>();
+            _bulk = Substitute.For<ILogBulkSet>();
+        }
+
+        [Test]
+        public void BULK_SHOULD_BE_RESET_WHEN_TIMER_ELAPSED()
+        {
+            //Arrange
+            var appender = new ElasticSearchAppender(_elasticClient, "index", "type", _timer, _tolerateCallsFactory, _bulk);
+            _bulk.ResetBulk().Returns(new List<InnerBulkOperation>());
+            
+            //Act
+            _timer.Elapsed += Raise.Event<EventHandler<object>>(this, null);
+
+            //Assert
+            _bulk.Received().ResetBulk();
+        }
+
+        [Test]
+        public void EMPTY_BULK_IS_NOT_INDEXED_WHEN_TIMER_ELAPSES()
+        {
+            //Arrange
+            var appender = new ElasticSearchAppender(_elasticClient, "index", "type", _timer, _tolerateCallsFactory, _bulk);
+            _bulk.ResetBulk().Returns(new List<InnerBulkOperation>());
+
+            //Act
+            _timer.Elapsed += Raise.Event<EventHandler<object>>(this, null);
+
+            //Assert
+            _elasticClient.DidNotReceiveWithAnyArgs().IndexBulk(null);
+            _elasticClient.DidNotReceiveWithAnyArgs().IndexBulkAsync(null);
+        }
+
+        [Test]
+        public void BULK_IS_INDEXED_ASYNC_WHEN_TIMER_ELAPSES()
+        {
+            //Arrange
+            var appender = new ElasticSearchAppender(_elasticClient, "index", "type", _timer, _tolerateCallsFactory, _bulk);
+            var bulk = new List<InnerBulkOperation> {new InnerBulkOperation()};
+            _bulk.ResetBulk().Returns(bulk);
+            appender.IndexAsync = true;
+            
+            //Act
+            _timer.Elapsed += Raise.Event<EventHandler<object>>(this, null);
+
+            //Assert
+            _elasticClient.Received().IndexBulkAsync(bulk);
+        }
+
+        [Test]
+        public void BULK_IS_INDEXED_WHEN_TIMER_ELAPSES()
+        {
+            //Arrange
+            var appender = new ElasticSearchAppender(_elasticClient, "index", "type", _timer, _tolerateCallsFactory, _bulk);
+            var bulk = new List<InnerBulkOperation> { new InnerBulkOperation() };
+            _bulk.ResetBulk().Returns(bulk);
+            appender.IndexAsync = false;
+
+            //Act
+            _timer.Elapsed += Raise.Event<EventHandler<object>>(this, null);
+
+            //Assert
+            _elasticClient.Received().IndexBulk(bulk);
+        }
+
+
+    }
+}
